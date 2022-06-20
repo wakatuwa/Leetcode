4f2833d2a29e53578f9dbd25125e66c02f527459
wanghan <51373861+wakatuwa@users.noreply.github.com>
2022-06-20 17:46:01 +0800

    Update 232.用栈实现队列.java

--- 2022_06/232.用栈实现队列.java
+++ 2022_06/232.用栈实现队列.java
@@ -7,24 +7,20 @@
 // @lc code=start
 class MyQueue {
 	//这题与225刚好是一对
-	//利用两个栈来实现,定义一个队首变量front
+	//利用两个栈来实现
 	//push操作，当s1为空时，直接将把新元素推入到s2，再把元素推入到s1
 	//当s1不为空时，先把s1元素取出放入s2，这时s2栈顶是s1栈底，s2栈底是s1栈顶
 	//再把新元素放到s2栈顶，最后再把s2元素依次压到s1中，这时新元素就跑到了s1栈底，满足队列
 
-	//pop操作，直接弹出，并把弹出的元素赋给front
+	//pop操作，先定义一个first变量保存s1的栈顶元素(队列的队首),然后弹出，最后返回保存的变量first
 	//peek操作，返回front
 	private Stack<Integer> s1 = new Stack<>();
 	private Stack<Integer> s2 = new Stack<>();
-	private int front;//代表队首
     public MyQueue() {
 
     }
     
     public void push(int x) {
-		if(s1.isEmpty()){
-			front = x;
-		}
 		while(!s1.isEmpty()){
 			s2.push(s1.pop());
 		}
@@ -35,12 +31,13 @@ class MyQueue {
     }
     
     public int pop() {
+		int first = s1.peek();
 		s1.pop();
-		return s1.peek();
+		return first;
     }
     
     public int peek() {
-		return front;
+		return s1.peek();
     }
     
     public boolean empty() {
